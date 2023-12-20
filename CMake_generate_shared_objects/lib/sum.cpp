8fcee24683316a98ff84f3741cdbe80c820f1c8d
Ankit Khandeparkar <ankit.khandeparkar@gmail.com>
2023-12-20 23:10:52 +0100

    Shared Objects (subdirectory) tutorial

---
+++ CMake_generate_shared_objects/lib/sum.cpp
@@ -0,0 +1,6 @@
+#include "calc/sum.h"
+
+int sum(int a, int b)
+{
+    return a + b;
+}
---
+++ CMake_generate_shared_objects/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include "calc/sum.h"
+
+using namespace std;
+
+int main(void)
+{
+    cout << "sum is " << sum(2,3) << endl;
+    return 0;
+}
